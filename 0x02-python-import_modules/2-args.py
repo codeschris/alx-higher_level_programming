8d357311a0b52858c5e03cd834511478dc7eb74e
codeschris <chrismlolwa42@outlook.com>
2023-05-11 11:23:31 -0700

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,8 @@
 from sys import argv
 
 if __name__ == '__main__':
-    argcount = len(argv) - 1
-    if (argcount == 0):
-        print("{} arguments.".format(argcount))
-    elif (argcount == 1):
-        print("{} argument:".format(argcount))
-        print("1: {}".format(argv[1]))
-    else:
-        print("{} arguments".format(argcount))
-        i = 1
-        for arg in argv[1:]:
-            print("{}: {}".format(i, arg))
-            i += 1
+    n = len(argv) - 1
+    print("{} argument{}{}"
+            .format(n, "" if n == 1 else "s", "." if n == 0 else ":"))
+    for i in range(n):
+        print("{}: {}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-import sys
-import hidden_4 as hidden
-
 if __name__ == '__main__':
+    import hidden_4 as hidden
     for name in dir(hidden):
         if name[0:2] != "__":
             print(name)
