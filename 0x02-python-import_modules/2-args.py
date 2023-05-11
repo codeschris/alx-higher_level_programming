cfe35638d073fce4a9353c0ad387329bb1ab396e
codeschris <chrismlolwa42@outlook.com>
2023-05-11 12:26:59 -0700

    correction on 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,12 @@ from sys import argv
 
 if __name__ == '__main__':
     n = len(argv) - 1
-    print("{} argument{}{}"
-            .format(n, "" if n == 1 else "s", "." if n == 0 else ":"))
-    for i in range(n):
-        print("{}: {}".format(i + 1, argv[i + 1]))
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(n))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
