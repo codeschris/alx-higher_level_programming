a54066492cd44a9e0399e80cbc54f1ee5e11e264
codeschris <chrismlolwa42@outlook.com>
2023-05-14 05:02:18 -0700

    checking elements using functions defined

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+
     for i in my_list:
-        if idx < 0 or idx > len(my_list):
-            return None
-        else:
-            i = idx
-    print(my_list[i])
+        i = idx
+        return(my_list[i]
