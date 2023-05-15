51739c1419efec6fbf9125292b1705fa5d55804b
codeschris <chrismlolwa42@outlook.com>
2023-05-15 06:27:28 -0700

    returning max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,5 @@ def max_integer(my_list=[]):
     if my_list = []:
         return None
 
-    for i in my_list:
-        if my_list[i] > my_list[(i + 1)]:
-            maxim = my_list[i]
-        return maxim
+    my_list.sort()
+    return my_list[-1]
